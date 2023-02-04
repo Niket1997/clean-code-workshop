099360e64199e963023c4564b208003623cd4b94
Niket1997 <aniket.mahangare1997@gmail.com>
2023-02-04 11:12:14 +0530

    added unit test

---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	Bytes = 1024
+)
--- main_test.go
+++ main_test.go
@@ -1,23 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/ginkgo/extensions/table"
-	. "github.com/onsi/gomega"
 )
 
-// Approach-1 with go testing library
-func TestToReadableSize(t *testing.T) {
-
-	got := toReadableSize(4000)
-	want := "4 KB"
-
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
-}
+const duplicateFilesDirectory = "duplicates_files_directory"
 
 // Approach-2 with go testing library with multiple multiple tests scenerios(Table Driven Test)
 type toReadableSizeTest struct {
@@ -41,61 +32,23 @@ func TestToReadableSizeMultiple(t *testing.T) {
 	}
 }
 
-// Approach-3 with go testing library with multiple multiple tests scenerios and name for each scenerio(Table Driven Test)
-func TestToReadableSizeMultipleApproachTwo(t *testing.T) {
-	tt := []struct {
-		name     string
-		input    int64
-		expected string
-	}{
-		{"byte_return", 125, "125 B"},
-		{"kilobyte_return", 1010, "1 KB"},
-		{"megabyte_return", 1988909, "1 MB"},
-		{"gigabyte_return", 29121988909, "29 GB"},
-		{"gigabyte_return", 890929121988909, "890 TB"},
-	}
+// TestTraverseDir: UT to test traverseDir method.
+func TestTraverseDir(t *testing.T) {
+	hashes := map[string]string{}
+	duplicates := map[string]string{}
+	var dupeSize int64
 
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			output := toReadableSize(tc.input)
-			if output != tc.expected {
-				t.Errorf("input %d, unexpected output: %s", tc.input, output)
-			}
-		})
-	}
+	cwd, err := os.Getwd()
+	assert.Nil(t, err)
+
+	directory := fmt.Sprintf("%s/%s", cwd, duplicateFilesDirectory)
+	dirFiles, err := ioutil.ReadDir(directory)
+	assert.Nil(t, err)
+
+	traverseDir(hashes, duplicates, &dupeSize, dirFiles, directory)
+
+	assert.Equal(t, 2, len(hashes))
+	assert.Equal(t, 2, len(duplicates))
+
+	assert.Equal(t, int64(120), dupeSize)
 }
-
-// Approach-4 with ginko ang gomega
-func TestToReadableSizeWithGinkoAndGomega(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "To readable size")
-}
-
-var _ = Describe("Main", func() {
-	Context("To readable size", func() {
-		It("should return in bytes if nbytes is less than 1000", func() {
-			result := toReadableSize(125)
-			Expect(result).To(Equal("125 B"))
-		})
-
-		It("should return in kilo bytes if nbytes is less than MB", func() {
-			result := toReadableSize(10000)
-			Expect(result).To(Equal("10 KB"))
-		})
-	})
-})
-
-// Approach-5 with ginko ang gomega(Table Driven Test)
-var _ = Describe("Main", func() {
-	Context("To readable size", func() {
-
-		DescribeTable("To readable size", func(nbytes int64, expectedResult string) {
-			actualResult := toReadableSize(nbytes)
-			Expect(actualResult).To(Equal(expectedResult))
-		},
-			Entry("should return in bytes if nbytes is less than 1000", int64(125), "125 B"),
-			Entry("should return in kilo bytes if nbytes is less than MB", int64(10000), "10 KB"),
-		)
-	})
-
-})
